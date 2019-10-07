7a372542ca6b28e1a1bdddd391e64bdc09598352
Bojan Joveski <bokicapk7@gmail.com>
2019-10-07 18:37:31 +0200

    Zacel sprojektom

---
+++ if_else.py
@@ -0,0 +1,17 @@
+ime = "Franci"
+
+if ime == "Miha":
+    print("Pozdravljen, Miha!")
+elif ime == "Franci":
+    print("Zdravo, Franci!")
+else:
+    print("Pozdravljen, neznanec")
+
+
+starost = 30
+if starost > 25:
+    print("star si vec kot 25")
+elif starost >= 18:
+    print("odrsel si")
+else:
+    print("Mlad si")
